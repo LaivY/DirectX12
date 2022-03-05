58d7fd470a4c249f05304155b9654ae4fe6f81a1
LaivY <dnsjdbstlr@naver.com>
2022-03-05 16:21:59 +0900

    FBXExporter
    완성

--- FBXExporter/FBXExporter.h
+++ FBXExporter/FBXExporter.h
@@ -1,15 +1,86 @@
 ﻿#pragma once
 #include <algorithm>
-#include <DirectXMath.h>
-#include <fbxsdk.h>
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <string>
 #include <vector>
+#include <DirectXMath.h>
+#include <fbxsdk.h>
 #include "utilities.h"
-using namespace DirectX;
 using namespace std;
+using namespace DirectX;
+
+#define GUN_SCALE_FACTOR 0.5
+
+struct Vertex
+{
+	Vertex() : position{}, normal{}, uv{}, boneIndices{}, boneWeights{}, materialIndex{ -1 } { }
+
+	XMFLOAT3	position;
+	XMFLOAT3	normal;
+	XMFLOAT2	uv;
+	int			materialIndex;
+	XMUINT4		boneIndices;
+	XMFLOAT4	boneWeights;
+};
+
+struct Material
+{
+	Material() : name{}, baseColor{} { }
+
+	string		name;
+	XMFLOAT4	baseColor;
+};
+
+struct CtrlPoint
+{
+	CtrlPoint() : position{} { }
+
+	XMFLOAT3					position;
+	vector<pair<int, double>>	weights;
+};
+
+struct Keyframe
+{
+	Keyframe() : frameNum{}
+	{
+		transformMatrix.SetIdentity();
+	}
+
+	FbxLongLong frameNum;
+	FbxAMatrix	transformMatrix;
+};
+
+struct Joint
+{
+	Joint() : parentIndex{ -1 }
+	{
+		globalBindposeInverseMatrix.SetIdentity();
+	}
+
+	string				name;
+	int					parentIndex;
+	FbxAMatrix			globalBindposeInverseMatrix;
+	vector<Keyframe>	keyframes;
+};
+
+struct Mesh
+{
+	Mesh() : isLinked{ false }, parentNode{ nullptr }, node{ nullptr }
+	{
+		transformMatrix.SetIdentity();
+	}
+
+	string				name;				// 이름
+	vector<Material>	materials;			// 재질
+	vector<CtrlPoint>	ctrlPoints;			// 제어점
+	vector<Vertex>		vertices;			// 정점
+	bool				isLinked;			// 링크 여부
+	FbxNode*			node;				// 자신 노드
+	FbxNode*			parentNode;			// 부모 노드
+	FbxAMatrix			transformMatrix;	// 메쉬 변환 행렬
+};
 
 class FBXExporter
 {
@@ -17,29 +88,31 @@ public:
 	FBXExporter();
 	~FBXExporter();
 
-	void Process(const string& inputFileName, const string& outputFileName);
+	void Process(const string& inputFileName, bool doExportMesh = true, bool doExportAnimation = true);
 	void LoadSkeleton(FbxNode* node, int index, int parentIndex);
 	void LoadMesh(FbxNode* node);
-	void LoadCtrlPoints(FbxNode* node);
-	void LoadAnimation(FbxNode* node);
-	void LoadVertices(FbxNode* node);
+	void LoadInfo(FbxNode* node, Mesh& mesh);
+	void LoadMaterials(FbxNode* node, Mesh& mesh);
+	void LoadCtrlPoints(FbxNode* node, Mesh& mesh);
+	void LoadAnimation(FbxNode* node, Mesh& mesh);
+	void LoadVertices(FbxNode* node, Mesh& mesh);
+	void ProcessLink();
 
-	int GetJointIndexByName(const string& name);
-	XMFLOAT3 GetNormal(FbxMesh* mesh, int controlPointIndex, int vertexCountIndex);
-	XMFLOAT2 GetUV(FbxMesh* mesh, int controlPointIndex, int vertexCountIndex);
+	int GetJointIndex(const string& name) const;
+	XMFLOAT3 GetNormal(FbxMesh* mesh, int controlPointIndex, int vertexCountIndex) const;
+	XMFLOAT2 GetUV(FbxMesh* mesh, int controlPointIndex, int vertexCountIndex) const;
+	int GetMaterial(FbxMesh* mesh, int polygonIndex) const;
 
-	void Export();
+	void ExportMesh() const;
+	void ExportAnimation() const;
 
 private:
+	string					m_inputFileName;	// 변환할 FBX 파일 이름
 	FbxManager*				m_manager;			// FBX 매니저
 	FbxScene*				m_scene;			// FBX 씬
 
-	string					m_inputFileName;	// 변환할 FBX 파일 이름
-	string					m_outputFileName;	// 결과 파일 이름
-
-	vector<Joint>			m_joints;			// 스켈레톤(조인트들)
-	vector<CtrlPoint>		m_ctrlPoints;		// 제어점들
-	vector<Vertex>			m_vertices;			// 정점들
+	vector<Mesh>			m_meshes;			// 메쉬
+	vector<Joint>			m_joints;			// 뼈
 	string					m_animationName;	// 애니메이션 이름
 	FbxLongLong				m_animationLength;	// 애니메이션 길이
 };
--- FBXExporter/utilities.h
+++ FBXExporter/utilities.h
@@ -1,81 +1,21 @@
 ﻿#pragma once
-#include <string>
-#include <vector>
 #include <DirectXMath.h>
 #include <fbxsdk.h>
 using namespace DirectX;
 using namespace std;
 
-// 참고한 깃허브 주소
-// https://github.com/lang1991/FBXExporter
-
-// 블렌딩 정보는 조인트 인덱스와 가중치를 갖는다.
-struct BlendingDatum
-{
-	BlendingDatum() : blendingIndex{}, blendingWeight{}
-	{
-
-	}
-
-	int		blendingIndex;
-	double	blendingWeight;
-};
-
-// 제어점은 좌표와 4개의 가중치 정보를 갖는다.
-struct CtrlPoint
-{
-	CtrlPoint() : position{}
-	{
-		blendingData.reserve(4);
-	}
-
-	XMFLOAT3				position;
-	vector<BlendingDatum>	blendingData;
-};
-
-// 키프레임은 몇 번째 프레임인지와 해당 프레임일 때 제어점의 모델 좌표계 변환 행렬을 갖는다.
-struct Keyframe
-{
-	Keyframe() : frameNum{}
-	{
-		globalTransformMatrix.SetIdentity();
-	}
-
-	FbxLongLong frameNum;
-	FbxAMatrix	globalTransformMatrix;
-};
-
-// 조인트는 이름, 부모 조인트의 인덱스, 로컬 -> 모델 좌표계 변환 행렬, 노드, 키프레임을 갖는다.
-struct Joint
-{
-	Joint() : name{}, parentIndex{ -1 }, node{ nullptr }
-	{
-		globalBindposeInverseMatrix.SetIdentity();
-	}
-
-	string				name;
-	int					parentIndex;
-	FbxAMatrix			globalBindposeInverseMatrix;
-	FbxNode*			node;
-	vector<Keyframe>	keyframes;
-};
-
-// 정점은 좌표, 노말, 텍스쳐 좌표, 블렌딩 정보를 갖는다.
-struct Vertex
-{
-	Vertex() : position{}, normal{}, uv{}
-	{
-
-	}
-
-	XMFLOAT3				position;
-	XMFLOAT3				normal;
-	XMFLOAT2				uv;
-	vector<BlendingDatum>	blendingData;
-};
-
 namespace Utilities
 {
+	inline string GetOnlyPath(const string& path)
+	{
+		size_t p{ path.find_last_of('/') };
+		return path.substr(0, p + 1);
+	}
+	inline string GetOnlyFileName(const string& path)
+	{
+		size_t p{ path.find_last_of('/') };
+		return path.substr(p + 1);
+	}
 	inline FbxAMatrix GetGeometryTransformation(FbxNode* node)
 	{
 		return FbxAMatrix(
@@ -84,7 +24,29 @@ namespace Utilities
 			node->GetGeometricScaling(FbxNode::eSourcePivot)
 		);
 	}
-
+	inline FbxAMatrix toFbxAMatrix(const XMFLOAT4X4 m)
+	{
+		FbxAMatrix result;
+		for (int i = 0; i < 4; ++i)
+			for (int j = 0; j < 4; ++j)
+				result[i][j] = m.m[i][j];
+		return result;
+	}
+	inline FbxAMatrix toFbxAMatrix(const XMMATRIX& m)
+	{
+		FbxAMatrix result;
+		XMFLOAT4X4 matrix;
+		XMStoreFloat4x4(&matrix, m);
+		return toFbxAMatrix(matrix);
+	}
+	inline XMFLOAT4X4 toXMFLOAT4X4(const FbxAMatrix& m)
+	{
+		XMFLOAT4X4 result;
+		for (int i = 0; i < 4; ++i)
+			for (int j = 0; j < 4; ++j)
+				result.m[i][j] = m[i][j];
+		return result;
+	}
 	inline XMFLOAT4 FbxVector4ToXMFLOAT4(const FbxVector4& v)
 	{
 		return XMFLOAT4{ 
@@ -94,12 +56,46 @@ namespace Utilities
 			static_cast<float>(v.mData[3])
 		};
 	}
-
-	inline void WriteFbxAMatrixToStream(ostream& s, const FbxAMatrix& m)
+	inline XMFLOAT4X4 Identity()
+	{
+		XMFLOAT4X4 result;
+		XMStoreFloat4x4(&result, XMMatrixIdentity());
+		return result;
+	}
+	inline XMFLOAT4X4 Transpose(const XMFLOAT4X4& m)
+	{
+		XMFLOAT4X4 result;
+		XMStoreFloat4x4(&result, XMMatrixTranspose(XMLoadFloat4x4(&m)));
+		return result;
+	}
+	inline void WriteToStream(ostream& s, const FbxAMatrix& m)
 	{
 		for (int i = 0; i < 4; ++i)
 			for (int j = 0; j < 4; ++j)
 				s << static_cast<float>(m.Get(i, j)) << " ";
-		s << endl;
+	}
+	inline void WriteToStream(ostream& s, const XMFLOAT4X4& m)
+	{
+		for (int i = 0; i < 4; ++i)
+			for (int j = 0; j < 4; ++j)
+				s << m.m[i][j] << " ";
+	}
+	inline void Print(const FbxAMatrix& m)
+	{
+		for (int i = 0; i < 4; ++i)
+		{
+			for (int j = 0; j < 4; ++j)
+				cout << m.Get(i, j) << " ";
+			cout << endl;
+		}
+	}
+	inline void Print(const XMFLOAT4X4& m)
+	{
+		for (int i = 0; i < 4; ++i)
+		{
+			for (int j = 0; j < 4; ++j)
+				cout << m.m[i][j] << " ";
+			cout << endl;
+		}
 	}
 }
